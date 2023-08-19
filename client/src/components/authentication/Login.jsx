d033ba0b28b21cd01027e418e5946be3f9afa6c1
sagar-biswas1 <bsagor99@gmail.com>
2023-08-20 00:00:26 +0600

    login page ui done

--- client/src/components/authentication/Login.jsx
+++ client/src/components/authentication/Login.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    VStack,
+  } from "@chakra-ui/react";
+  import React, { useState } from "react";
 const Login = () => {
-    return (
-        <div>
-            login
-        </div>
-    );
+    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const submitHandler = () => {
+    console.log("hello");
+  };
+  return (
+    <VStack spacing="5px" color="black">
+      
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <Input
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormControl>
+      
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
